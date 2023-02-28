af5fc3a7721e15ea9c0fcf5c77be6c949461b0d6
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-28 17:17:02 +0000

    translate templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" a simple flask application to demonstrate different languages support"""
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """configure available languages in this application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+def get_locale():
+    """get the user's locale"""
+    return request.accept_languages.best_match(["en", "fr"])
+
+
+@app.route('/')
+def root():
+    """return the root of this application"""
+    return(render_template('3-index.html'))
+
+
+if __name__ == '__main__':
+    app.config.from_object(Config)
+    app.run(host='0.0.0.0')
