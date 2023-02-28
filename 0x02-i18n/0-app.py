7041f761248ebadb64cd724e3125d8bbde88fbcf
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-28 12:53:12 +0000

    check pycodestyle errors

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
     """return the root of this application"""
     return(render_template('0-index.html'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
