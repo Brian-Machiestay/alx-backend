915275e6de28485cc109a3ba811ec7bb898ed416
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-01 02:52:03 +0000

    add decorator to get user local

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -15,6 +15,7 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+@babel.localeselector
 def get_locale():
     """get the user's locale"""
     return request.accept_languages.best_match(["en", "fr"])
